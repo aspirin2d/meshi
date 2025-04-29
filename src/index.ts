aed94269a66ced8166ca0a9efdf61d8f13b9a803
Aspirin2D@MacbookAir <aspirin2ds@gmail.com>
2025-04-29 14:00:59 +0800

    add utils

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
+import { auth } from "./auth";
+import { profile } from "./profile";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/", (c) => {
-	return c.text(`hello, ${c.env.TEST}.`);
+app.get("/hello", (c) => {
+	return c.json({ message: "hello, world" });
 });
+
+app.route("/auth", auth);
+app.route("/profile", profile);
 
 export default app;
